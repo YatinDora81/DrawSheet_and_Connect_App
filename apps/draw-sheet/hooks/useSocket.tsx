5a37dd647d1ae9c5dc2231f134756cee21a43856
YatinDora81 <yatin.dora81@gmail.com>
2025-09-08 23:28:10 +0530

    fix drawsheet build

--- apps/draw-sheet/hooks/useSocket.tsx
+++ apps/draw-sheet/hooks/useSocket.tsx
@@ -32,7 +32,7 @@ export const SocketProvider = ({ children }: { children: ReactNode }) => {
             const token = getCookie("authToken");
 
             await toast.promise(
-                new Promise((resolve, reject) => {
+                new Promise<string>((resolve, reject) => {
                     const ws = new WebSocket(BASE_WS_URL + `?token=${token}`);
 
                     ws.onopen = () => {
--- apps/http-server/src/utils/sendTokenAndCookie.ts
+++ apps/http-server/src/utils/sendTokenAndCookie.ts
@@ -6,6 +6,10 @@ export const sendTokenAndCookie = (res: Response, data: { user_id: string, email
     const token = jwt.sign(data, JWT_SECRET, { expiresIn: '15d' });
     // res.cookie("authToken", token, { maxAge: 15 * 24 * 60 * 60 , sameSite : "none" })
     res.setHeader("Set-Cookie", `authToken=${token}; Path=/; Secure; SameSite=None; Max-Age=${15 * 24 * 60 * 60}`);
+    // res.setHeader(
+    //     "Set-Cookie",
+    //     `authToken=${token}; Path=/; Secure; HttpOnly; SameSite=None; Domain=.yatindora.xyz; Max-Age=${15 * 24 * 60 * 60}`
+    // );
 
 
 }
