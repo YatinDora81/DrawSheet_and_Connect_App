72c5bded30f656c9cde2c94819e5bea194e696c3
YatinDora81 <yatin.dora81@gmail.com>
2025-09-14 23:15:21 +0530

    Update nodemailer configuration to use secure port 465 for email transport

--- apps/http-server/src/config/nodemailer.ts
+++ apps/http-server/src/config/nodemailer.ts
@@ -9,8 +9,8 @@ const getTransporter = async () => {
     config()
     return nodemailer.createTransport({
         host: process.env.MAIL_HOST,
-        port: 587,
-        secure: false, // true for 465, false for other ports
+        port: 465,
+        secure: true, // true for 465, false for other ports
         auth: {
             user: process.env.MAIL_USER,
             pass: process.env.MAIL_PASS,
